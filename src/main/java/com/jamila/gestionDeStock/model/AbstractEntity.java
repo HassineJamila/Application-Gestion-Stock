4b5375a9ad97e728d0e577ee343c28ba160be3f3
Hcine Jamila <hcine.jamila.info@gmail.com>
2022-10-06 11:09:10 +0100

    NewCommit

--- src/main/java/com/jamila/gestionDeStock/model/AbstractEntity.java
+++ src/main/java/com/jamila/gestionDeStock/model/AbstractEntity.java
@@ -1,2 +1,32 @@
-package com.jamila.gestionDeStock.model;public class AbstractEntity {
+package com.jamila.gestionDeStock.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@MappedSuperclass()
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @CreatedDate
+    @JsonIgnore
+    @Column(name="createdDate",nullable = false)
+    private Date createdDate;
+
+    @JsonIgnore
+    @LastModifiedDate
+    @Column(name="lastModifiedDate",nullable = false)
+    private Date lastModifiedDate;
+
 }
